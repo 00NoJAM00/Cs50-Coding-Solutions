17a77644fb6dd4503788fd853aa3a48730a4cdde
00NoJAM00 <93239533+00NoJAM00@users.noreply.github.com>
2022-10-13 22:28:07 -0400

    Update Cs50 Python Mario.py

--- Cs50 Python Mario.py
+++ Cs50 Python Mario.py
@@ -3,18 +3,19 @@ from cs50 import get_int
 #range from 1-8
 #Has to be a pyramid and the pyramid right facing
 
-#get input from user.
+#get input from user until correct
 while True:
     n = get_int("Enter Height: ")
-    if n >= 1 or n <= 8:
+    if n > 0 and n < 9:
         break
-        
+
 def get_height():
-    for i in range(n):
-        for s in range(n - i - 1, 0 , -1):
-            print(" ", end="")
-        for hash in range(i + 1):
-            print("#", end="")
+    for i in range(0, n, 1):
+        for j in range(0, n , 1):
+            if (i + j < n -1):
+                print(" ", end="")
+            else:
+                print("#", end="")
         print()
 
 get_height()
@@ -22,16 +23,15 @@ get_height()
 
 
 
-#YOU COULD ALSO WRITE THE CODE AS THUS BUT IT IS A LOT MORE INFORMATION AND int(input("Height: ")) is not accepted for the cs50 check BUT it seemed to be correct code.
-# Or i should say it does take the input and gives a correct output: But go for the top code for this problem.
+#You could also write the code like thus below:
+#Keep in mind the code above uses the get_int so you need to import that from cs50, if you dont want to then just use int(input()).
 
-#get input from user:
 while True:
     try:
         n = int(input("Enter Height: "))
-        if n >= 1 or n <= 8:
+        if n >= 1 and n <= 8: 
             break
-        elif n <= 0 or n >= 9:
+        elif n <= 0 and n >= 9:
             print("Please enter a number from 1 to 8!")
     except ValueError:
             print("Please enter a number!")
